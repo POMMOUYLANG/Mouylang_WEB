ab86fea212194cbdb174ad8bf4a0d4dfe3ffe506
POM-MOUYLANG <126035070+POMMOUYLANG@users.noreply.github.com>
2023-08-14 10:42:03 +0700

    update_vue

--- VUE_HTML/assets/js/app.js
+++ VUE_HTML/assets/js/app.js
@@ -1,8 +1,12 @@
 const app = Vue.createApp({
-    hello: 'Hello',
+    // template: '<h1>Welcome To Vue JS and Name: {{firstName}}</h1>',
     data(){
         return{
-            firstName: 'Jonh'
+            firstName: 'POM',
+            lastName: 'Mouylang',
+            email: 'mouylangpom@gmail.com',
+            gender: 'Female',
+            picture: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoWt54eC2lBZybJaeKr3lWDeIUrq_q7Imecg&usqp=CAU',
         }
     }
 })
