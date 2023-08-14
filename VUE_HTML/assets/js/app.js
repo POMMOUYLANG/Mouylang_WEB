c8731f28389f475efb755b6c12f0cb7f1d825575
POM-MOUYLANG <126035070+POMMOUYLANG@users.noreply.github.com>
2023-08-14 14:01:03 +0700

    update

--- VUE_HTML/assets/js/app.js
+++ VUE_HTML/assets/js/app.js
@@ -8,7 +8,22 @@ const app = Vue.createApp({
             gender: 'Female',
             picture: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoWt54eC2lBZybJaeKr3lWDeIUrq_q7Imecg&usqp=CAU',
         }
-    }
+    },
+    methods: {
+        async getUser(){
+            // console.log(this.firstName)
+            const res = await fetch('https://randomuser.me/api')
+            const {result} = await res.json()
+
+            console.log(result)
+            this.firstName = 'SAM',
+            this.lastName =  'Smith',
+            this.email = 'smithsam@gmail.com',
+            this.gender = 'Male',
+            this.picture = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlnp100mRs3ONsl3K-RyfLGSHqFE-xBxuxeQ&usqp=CAU'
+            }
+    },
 })
+
 
 app.mount('#app');
