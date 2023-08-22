5db4aec755e6c5697161b3376f18ccb081aca887
POM-MOUYLANG <126035070+POMMOUYLANG@users.noreply.github.com>
2023-08-22 21:38:56 +0700

    JavaScript Objects

---
+++ JAVASCRIPT/assets/js/Day5.js
@@ -0,0 +1,11 @@
+
+const car = {type:"Fiat", model:"500", color:"white"};
+document.getElementById("demo1").innerHTML = "The car type is " + car.type;
+
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 50,
+    eyeColor: "blue"
+  };
+  document.getElementById("demo2").innerHTML = person.firstName + " is " + person.age + " years .";
