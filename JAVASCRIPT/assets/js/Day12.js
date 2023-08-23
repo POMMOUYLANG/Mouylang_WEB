0f418b73c9d3045a5a41fd390876e4442359498c
POM-MOUYLANG <126035070+POMMOUYLANG@users.noreply.github.com>
2023-08-23 19:13:27 +0700

    JavaScript Number Properties

---
+++ JAVASCRIPT/assets/js/Day12.js
@@ -0,0 +1,22 @@
+let x1 = Number.EPSILON;
+document.getElementById("demo1").innerHTML = x1;
+
+let x2 = Number.MAX_VALUE;
+document.getElementById("demo2").innerHTML = x2;
+
+let x3 = Number.MIN_VALUE;
+document.getElementById("demo3").innerHTML = x3;
+
+let x4 = Number.MAX_SAFE_INTEGER;
+document.getElementById("demo4").innerHTML = x4;
+
+let x5 = Number.MIN_SAFE_INTEGER;
+document.getElementById("demo5").innerHTML = x5;
+
+let x6 = Number.POSITIVE_INFINITY;
+document.getElementById("demo6").innerHTML = x6;
+
+let x7 = Number.NEGATIVE_INFINITY;
+document.getElementById("demo7").innerHTML = x7;
+
+document.getElementById("demo8").innerHTML = Number.NaN;
