d919a3300c94d885c29a2a9e831063462e913a47
POM-MOUYLANG <126035070+POMMOUYLANG@users.noreply.github.com>
2023-08-23 13:59:02 +0700

    JavaScript Template Literals

---
+++ JAVASCRIPT/assets/js/Day9.js
@@ -0,0 +1,10 @@
+let text = `Hello World!`;
+document.getElementById("demo1").innerHTML = text;
+
+let index1 = `He's often called "Johnny"`;
+document.getElementById("demo2").innerHTML = index1;
+
+let firstName = "John";
+let lastName = "Doe";
+let index2 = `Welcome ${firstName}, ${lastName}!`;
+document.getElementById("demo3").innerHTML = index2;
